cda0390c368f4460e69910cfed773a670c33819b
Yulian2405 <juliangalvez1524@gmail.com>
2025-05-25 00:10:13 -0600

    Se corrigieron errores de Login

--- ProyectoFinal/Program.cs
+++ ProyectoFinal/Program.cs
@@ -19,7 +19,7 @@ namespace ProyectoFinal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmServicios());
+            Application.Run(new FrmLogin1());
         }
     }
 }
